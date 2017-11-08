593c0670f89dc9cc127e72ab14098f6a4e23e4bf
akiva <akiva.bloch@bluevine.com>
2017-11-08 22:56:19 +0200

    add words and fix bugs

--- run.py
+++ run.py
@@ -16,17 +16,17 @@ guesses = {
 	5: ['כ', 'ל', 'מ', 'י', 'נ', 'ס'],
 	6: ['כ', 'ל', 'מ', 'ז', 'ח', 'ט', 'ב', 'ג', 'ד', 'ה', 'ק', 'ש'],
 	7: ['ק', 'ר', 'ש', 'ג', 'ד', 'ה', 'ס', 'ע', 'פ', 'ז', 'ח', 'ט', 'ב'],
-	8: [],
-	9: [],
-	10: [],
-	11: [],
-	12: [],
-	13: [],
+	8: ['ש', 'ת', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס',],
+	9: ['א', 'ב', 'ט', 'י', 'כ', 'ל', 'מ', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת'],
+	10: ['א', 'ב', 'צ', 'ק', 'ר', 'ש'],
+	11: ['ז', 'ח', 'ט', 'י', 'ל', 'מ', 'נ'],
+	12: ['צ', 'ק', 'ר', 'ש'],
+	13: ['ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'ק', 'ר', 'ש'],
 	14: [],
-	15: [],
-	16: [],
-	17: [],
-	18: [],
+	15: ['פ', 'צ', 'ק', 'ר', 'ש', 'ת'],
+	16: ['א', 'ב', 'ג', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'ע', 'פ', 'צ', ],
+	17: ['ב', 'ג', 'ד', 'י', 'כ', 'ל', 'מ', 'נ', 'ס'],
+	18: ['כ', 'ל', 'מ', 'ס', 'ע', 'פ'],
 	19: [],
 	20: [],
 	21: [],
@@ -46,14 +46,18 @@ def generate_assumption():
 	assum = dict()
 	for letter, guess in filled_guesses.items():
 		possible_letters = unguessed.intersection(guess)
+		if not possible_letters:
+			return None
 		guessed_letter = sample(possible_letters, 1)[0]
-		possible_letters.remove(guessed_letter)
+		unguessed.remove(guessed_letter)
 		assum[letter] = guessed_letter
 	return assum
 
 
 def get_assumption(assum):
 	"""assumption is an ordered list of the alphabeta so that 01 is the first item, 02 is the seconds etc."""
+	if not assum:
+		return
 	ret = ''
 	for word in text:
 		for letter in word:
@@ -61,6 +65,9 @@ def get_assumption(assum):
 		ret += " "
 	return ret[::-1]
 
-print(generate_assumption())
+for i in range(1000):
+	guess = get_assumption(generate_assumption())
+	if guess:
+		print(guess)
 
 # ipdb.set_trace()
